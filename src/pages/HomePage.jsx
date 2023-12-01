ab6e05639a6c91caa65b8827b45ea8e82d4dc3e8
Bartas Dausynas <bartas3@gmail.com>
2023-12-01 16:19:35 +0200

    some commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import style from "./HomePage.module.css";
-import { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalContext';
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 export function HomePage() {
   const { isLogined } = useContext(GlobalContext);
 
-  const text = !isLogined? "Please sign in!" : "You're welcome";
+  const text = !isLogined ? "Please sign in!" : "You're welcome";
 
   return (
     <>
-      <p className={style.text}>{text}</p>
+      <p className={style.text}>{text}MEH MEH</p>
     </>
   );
 }
